e9b3dbd19011484b80ddcf8a3e573a4a1649384b
easydong19 <ymh1353@naver.com>
2022-10-09 22:29:45 +0900

    first init

---
+++ src/main/java/dev/be/foodeat/FoodEatApplication.java
@@ -0,0 +1,13 @@
+package dev.be.foodeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/foodeat/FoodEatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.foodeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
